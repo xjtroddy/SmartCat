119f492afa6f618c4b18b296aa34e4b56b54fc39
薛吉霆 <xuejiting@xuejitingdeMacBook-Pro.local>
2016-06-01 00:44:04 +0800

    nothing important

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '老婆' });
+    res.render('index', { title: 'Main' });
 });
 
+router.get('/reg', function(req, res) {
+    res.render('reg', { title: 'Register' });
+});
+
+router.post('/reg', function(req, res) {
+
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', { title: 'Login' });
+});
+
+router.post('/login', function(req, res) {
+
+});
+
+router.get('/post', function(req, res) {
+    res.render('post', { title: 'Publish' });
+})
+
+router.post('/post', function(req, res) {
+
+});
+
+router.get('/logout', function(req, res) {
+
+});
 module.exports = router;
